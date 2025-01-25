cc5e8f509e4ef50ab8fdc498e07576ba98e45c0a
Parmeet-Singh1313 <147844266+Parmeet-Singh1313@users.noreply.github.com>
2025-01-25 20:23:28 +0530

    page.jsx

--- app/course/[courseId]/page.jsx
+++ app/course/[courseId]/page.jsx
@@ -25,7 +25,7 @@ function Course({ params }) {
     return (
         <div>
             <Header />
-            <div className="px-10 p-10 md:px-20 lg:px-44">
+            <div className="px-10 p-10 md:px-20 lg:px-44 mt-20">
                 <CourseBasicInfo course={course} edit={false} />
 
                 <CourseDetail course={course} />
